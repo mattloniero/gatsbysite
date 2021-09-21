f1684396a888fc5f5b13059ec248ff6806de1339
mattloniero <mloniero@gmail.com>
2021-09-20 22:41:00 -0700

    staticQuery Site Metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Website",
+    siteUrl: "https://mattloniero.gatsbyjs.io",
+    title: "Matt Loniero Website",
   },
   plugins: [
     {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,25 @@
 import * as React from "react"
+import { useStaticQuery, graphql } from 'gatsby'
 import Header from "../components/Header/Header"
 import Layout from "../components/Layout/Layout"
 
 const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query SiteMetadata {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
   return (
-    <Layout pageTitle="Matt Loniero Website Home">
+    <Layout pageTitle={ data.site.siteMetadata.title }>
       <Header />
     </Layout>
   )
 }
+
+
 
 export default IndexPage
